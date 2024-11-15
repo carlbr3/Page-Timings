9dd482fe5fc0a3325d515cfd11693dcc8962e144
carlbr3 <brandon.carlson2089@gmail.com>
2024-11-14 22:31:01 -0700

    java changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,15 +2,24 @@
 
 const showTitle = document.getElementById('show-title');
 const showSubmit = document.getElementById('show-submit');
+const errorMessage = document.getElementById('error');
 let titleArray = [];
 
 
 function saveLocal (event) {
     event.preventDefault();
-    titleArray = JSON.parse(localStorage.getItem('show-titles'));
-    titleArray.push(showTitle.value);
-    localStorage.setItem('show-titles', JSON.stringify(titleArray)); 
-    showTitle.value = "";
+    if (showTitle.value === null || showTitle.value === "") {
+        errorMessage.classList.replace('d-none','d-block');
+    } else {
+        titleArray = JSON.parse(localStorage.getItem('show-titles'));
+        if (titleArray === null) {
+            titleArray = [];
+        }
+        titleArray.push(showTitle.value);
+        localStorage.setItem('show-titles', JSON.stringify(titleArray)); 
+        showTitle.value = "";
+    }
 }
 
 showSubmit.addEventListener('click', saveLocal);
+
